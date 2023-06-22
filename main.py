e9d814033cb6f877711c11c631a139c54ae39b7c
Nikita Karavaev <nikitakaravaev@mail.ru>
2023-06-22 23:11:39 +0300

    add_show_popup_function_2

--- main.py
+++ main.py
@@ -362,7 +362,7 @@ class EmailsenderApp(App):
                 try:
                     mgs = MIMEMultipart()
                     mgs['From'] = FROM_E_MAIL
-                    mgs['To'] = TO_E_MAIL, FROM_E_MAIL
+                    mgs['To'] = ', '.join([TO_E_MAIL, FROM_E_MAIL])
                     mgs['Subject'] = SUBJECT
                     body = f'Добрый день!\n' \
                            f'\n' \
